d8f2bc8285cac33b0d5e59b67d74f99ee86d04d6
bbeda <bogdan.beda@hotmail.com>
2016-11-10 07:59:56 +0200

    - implemented percentage distribution

--- PercentageDistribution/Program.cs
+++ PercentageDistribution/Program.cs
@@ -10,6 +10,46 @@ namespace PercentageDistribution
     {
         static void Main(string[] args)
         {
+            var options = new[]
+            {
+                new Option() { ExpectedPercentage=10 },
+                new Option() { ExpectedPercentage=25 },
+                new Option() { ExpectedPercentage=35 },
+                new Option() { ExpectedPercentage=30 }
+            };
+
+            var totalCount = 0;
+            int value = 0;
+            while ((value = int.Parse(Console.ReadLine())) != 0)
+            {
+                for (int ix = 0; ix < value; ix++)
+                {
+                    var errors = options.Select((o, sx) =>
+                    {
+                        var currentPercentage = totalCount == 0 ? 0 : (double)o.Count / totalCount * 100;
+                        var currentError = o.ExpectedPercentage - currentPercentage;
+                        return new { Error = currentError, Index = sx };
+
+                    }).ToArray();
+
+                    var selectedOption = errors.Where(er => er.Error >= 0).OrderBy(er => er.Error).Last();
+                    options[selectedOption.Index].Count++;
+                    totalCount++;
+                }
+
+                Console.WriteLine($"Total count{totalCount}");
+                foreach (var ops in options)
+                {
+                    Console.WriteLine($"{ops.ExpectedPercentage}%=>{ops.Count} {ops.Count / (double)totalCount * 100}%");
+                }
+            }
+        }
+
+        class Option
+        {
+            public int ExpectedPercentage { get; set; }
+
+            public int Count { get; set; }
         }
     }
 }
